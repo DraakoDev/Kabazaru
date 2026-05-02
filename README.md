05936e28578cd69a446853e31d37e644887341a1
draako <draakox.dev@gmail.com>
2026-05-02 15:43:54 -0500

    Feat: Script SQL

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Consesionario De Vehiculos
 
-## Diagrama ER-3FN V2
+## Diagrama ER-3FN V2.1
 
 ![](./image.png)
--- script.sql
+++ script.sql
@@ -1,62 +1,142 @@
+CREATE TABLE Empresa (
+    Nit VARCHAR(20) PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Direccion VARCHAR(200) NOT NULL,
+    Telefono VARCHAR(20) NOT NULL
+    Correo VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE RegistroEmpresa (
+    Nit VARCHAR(20) NOT NULL PRIMARY KEY,
+    FechaRegistro DATE NOT NULL,
+    TipoEmpresa VARCHAR(50) NOT NULL,
+    FOREIGN KEY (Nit) REFERENCES Empresa(Nit)
+);
+
 CREATE TABLE Concesionario (
-    nif varchar(20) PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    direccion VARCHAR(255) NOT NULL,
-    telefono VARCHAR(20) NOT NULL
+    Nit VARCHAR(20) NOT NULL PRIMARY KEY,
+    FOREIGN KEY (Nit) REFERENCES RegistroEmpresa(Nit)
 );
 
 CREATE TABLE ServicioOficial (
-    nif varchar(20) PRIMARY KEY,
-    consesionario_nif varchar(20) NOT NULL,
-    nombre VARCHAR(255) NOT NULL,
-    direccion VARCHAR(255) NOT NULL,
-    telefono VARCHAR(20) NOT NULL,
-    FOREIGN KEY (consesionario_nif) REFERENCES Concesionario(nif)
+    Nit VARCHAR(20) NOT NULL PRIMARY KEY,
+    ConcesionarioNit VARCHAR(20) NOT NULL,
+    FOREIGN KEY (Nit) REFERENCES RegistroEmpresa(Nit)
+    FOREIGN KEY (ConcesionarioNit) REFERENCES Concesionario(Nit)
+)
+
+CREATE TABLE Persona (
+    Cedula VARCHAR(20) PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Apellido VARCHAR(100) NOT NULL,
+    Direccion VARCHAR(200) NOT NULL,
+    Telefono VARCHAR(20) NOT NULL,
+    Correo VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Vendedor (
+    Cedula VARCHAR(20) NOT NULL PRIMARY KEY,
+    RegistroEmpresaNit VARCHAR(20) NOT NULL,
+    FOREIGN KEY (Cedula) REFERENCES Persona(Cedula),
+    FOREIGN KEY (RegistroEmpresaNit) REFERENCES RegistroEmpresa(Nit)
+);
+
+CREATE TABLE Usuario (
+    Cedula VARCHAR(20) NOT NULL PRIMARY KEY,
+    NombreUsuario VARCHAR(50) NOT NULL,
+    Contrasena VARCHAR(100) NOT NULL,
+    Tipo VARCHAR(20) NOT NULL,
+    FOREIGN KEY (Cedula) REFERENCES Persona(Cedula),
 );
 
 CREATE TABLE Marca (
-    nombre VARCHAR(255) PRIMARY KEY
-);
-
-CREATE TABLE MarcaConcesionario (
-    marca_nombre VARCHAR(255) NOT NULL,
-    concesionario_nif VARCHAR(20) NOT NULL,
-    PRIMARY KEY (marca_nombre, concesionario_nif),
-    FOREIGN KEY (marca_nombre) REFERENCES Marca(Nombre),
-    FOREIGN KEY (concesionario_nif) REFERENCES Concesionario(nif)
+    Nombre VARCHAR(50) PRIMARY KEY
 );
 
 CREATE TABLE Modelo (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre_marca VARCHAR(255) NOT NULL,
-    nombre VARCHAR(255) NOT NULL,
-    ficha_tecnica_id INT NOT NULL,
-    FOREIGN KEY (nombre_marca) REFERENCES Marca(nombre)
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    MarcaNombre VARCHAR(50) NOT NULL,
+    Nombre VARCHAR(50) NOT NULL,
+    Precio DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (MarcaNombre) REFERENCES Marca(Nombre)
+);
+
+CREATE TABLE Automovil (
+    NumeroBastidor VARCHAR(20) PRIMARY KEY,
+    ModeloId INT NOT NULL,
+    Estado VARCHAR(20) NOT NULL,
+    RegistroEmpresaNit VARCHAR(20) NOT NULL,
+    FOREIGN KEY (ModeloId) REFERENCES Modelo (Id),
+    FOREIGN KEY (RegistroEmpresaNit) REFERENCES RegistroEmpresa(Nit)
+);
+
+CREATE TABLE Color (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE FichaTecnica (
-    id_modelo INT PRIMARY KEY,
-    cilindraje varchar(20) NOT NULL,
-    potencia varchar(20) NOT NULL,
-    torque varchar(20) NOT NULL,
-    motor varchar(255) NOT NULL,
-    combustible varchar(255) NOT NULL,
-    carroceria varchar(255) NOT NULL,
-    color varchar(255) NOT NULL,
-    FOREIGN KEY (id_modelo) REFERENCES Modelo(id)
+    ModeloId INT NOT NULL PRIMARY KEY,
+    Cilindraje DECIMAL(10, 2) NOT NULL,
+    Potencia DECIMAL(10, 2) NOT NULL,
+    Torque DECIMAL(10, 2) NOT NULL,
+    Motor VARCHAR(50) NOT NULL,
+    Combustible VARCHAR(50) NOT NULL,
+    Carroceria VARCHAR(50) NOT NULL,
+    Color INT NOT NULL,
+    FOREIGN KEY (ModeloId) REFERENCES Modelo(Id)
+    FOREIGN KEY (Color) REFERENCES Color(Id);
 );
 
 CREATE TABLE Accesorio (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(255) NOT NULL,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50) NOT NULL,
 );
 
 CREATE TABLE Equipamiento (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_modelo INT NOT NULL,
-    accsorio_id INT NOT NULL,
-    es_extra BOOLEAN NOT NULL,
-    precio DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (id_modelo) REFERENCES Modelo(id),
-    FOREIGN KEY (accsorio_id) REFERENCES Accesorio(id)
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    ModeloId INT NOT NULL,
+    AccesorioId INT NOT NULL,
+    EsExtra BOOLEAN NOT NULL,
+    Precio DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (ModeloId) REFERENCES Modelo(Id),
+    FOREIGN KEY (AccesorioId) REFERENCES Accesorio(Id)
+);
+
+CREATE TABLE Venta (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    NumeroBastidor VARCHAR(20) NOT NULL UNIQUE,
+    CedulaVendedor VARCHAR(20) NOT NULL,
+    FechaEntrega DATE NOT NULL,
+    FechaVenta DATE NOT NULL,
+    MatriculaAsignada VARCHAR(20) NOT NULL,
+    EsEncargo BOOLEAN NOT NULL,
+    MetodoPago VARCHAR(50) NOT NULL,
+    PrecioVenta DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (NumeroBastidor) REFERENCES Automovil(NumeroBastidor),
+    FOREIGN KEY (CedulaVendedor) REFERENCES Vendedor(Cedula),
+);
+
+CREATE TABLE ExtraVenta (
+    VentaId INT NOT NULL, 
+    EquipamientoId INT NOT NULL,
+    Precio DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (VentaId, EquipamientoId),
+    FOREIGN KEY (VentaId) REFERENCES Venta(Id),
+    FOREIGN KEY (EquipamientoId) REFERENCES Equipamiento(Id)
+);
+
+CREATE TABLE Descuento (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50) NOT NULL,
+    Porcentaje DECIMAL(5, 2) NOT NULL, 
+    Descripcion VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE DescuentoAplicado (
+    AutomovilId VARCHAR(20) NOT NULL,
+    DescuentoId INT NOT NULL,
+    PRIMARY KEY (AutomovilId, DescuentoId),
+    FOREIGN KEY (AutomovilId) REFERENCES Automovil(NumeroBastidor),
+    FOREIGN KEY (DescuentoId) REFERENCES Descuento(Id)
 );
