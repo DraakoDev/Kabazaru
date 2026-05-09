cf2f6010af9b1916450146d51237334eb4a0f701
AntonySisas <antonyjoelriverae10@gmail.com>
2026-05-08 21:19:39 -0500

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,1 +1,9 @@
-sasdsad
+import axios from "axios";
+
+const API = "http://localhost:3000";
+
+export const loginRequest = async (data) => {
+  return await axios.post(`${API}/login`, data, {
+    withCredentials: true
+  });
+};
